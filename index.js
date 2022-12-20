2ba8f47d659f5eb289f33cc20dba94699542403a
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-20 18:31:52 +0200

    dom manipulation codes

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let lucky = document.querySelector("h2");
+lucky.style.color = 'blue';
+
+let belie = document.getElementsByClassName('list');
+console.log(belie);
+for (let i=0;i<belie.length;i++){
+    belie[i].style.color = 'red'
+    if(i==3){
+        
+    }
+}
