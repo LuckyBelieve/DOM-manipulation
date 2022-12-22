b0b81594c1c636d4d57a58bf10dc6d8c3e903118
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-22 21:19:19 +0200

    this is upgraded index.js in DOM manipulation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ lucky.style.color = 'blue';
 
  let ul = document.querySelector('ul')
  let li = document.createElement('li')
- let text = document.createTextNode("emancipation")
+ let text = document.createTextNode("Emancipation")
  li.setAttribute('class','list')
  ul.append(li);
  li.append(text);
@@ -24,6 +24,7 @@ lucky.style.color = 'blue';
 // newList.remove();
 
 //creating element 
+
 let newUl = document.querySelector('ul')
 let newList = document.createElement('li')
 newList.innerText = 'Day shift';
@@ -41,3 +42,35 @@ for (let i=0;i<belie.length;i++){
     belie[i].style.color = 'darkblue'
 }
 
+//The codes below are here to let us deal with the parent and children hierachy
+
+let unList = document.querySelector('ul');
+
+let believe = document.firstElementChild;
+console.log(believe);
+
+console.log(unList.childNodes)
+console.log(unList.firstChild)
+
+//coloring the lastchild of our unordered list
+// we may say
+
+unList.lastChild.style.color = 'red';
+
+//these codes were used to color the background of all the childnodes of 
+//list element
+
+let mary = unList.childNodes;
+
+for(let c = 0;c<mary.length;c++){
+    mary[c].style.backgroundColor = 'violet'
+}
+
+//instead of using childnodes we can also use children and we may perform the same action
+
+let happy = unList.children;
+for(let c = 0;c<happy.length;c++){
+    happy[c].style.backgroundColor = 'gray'
+}
+//accesing the nodes using their respective indices
+console.log(unList.childNodes[0]);
