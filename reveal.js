c37cbb80e257bf7e0b7051fcbe67d97123388e76
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-23 20:18:20 +0200

    updates on reveal.js

--- reveal.js
+++ reveal.js
@@ -9,10 +9,12 @@ let hiddenContent = document.querySelector('.hidden-content');
 function revealContent(){
 
     if(hiddenContent.classList.contains('reveal-btn')){
+
         hiddenContent.classList.remove('reveal-btn');
     }else{
         hiddenContent.classList.add('reveal-btn')
     }
 }
+console.log(hiddenContent);
 
 revealBtn.addEventListener("click",revealContent);
