51c559e358f3ace1e8a288ee60c82dac08f31764
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2023-01-06 22:04:58 +0200

    updates on revealing hidden content using js

--- reveal.js
+++ reveal.js
@@ -13,7 +13,7 @@ function revealContent(){
         hiddenContent.classList.remove('reveal-btn');
     }else{
         hiddenContent.classList.add('reveal-btn')
-        // revealBtn.classList.add('.remove')
+        revealBtn.classList.add('.remove')
     }
 }
 console.log(hiddenContent);
